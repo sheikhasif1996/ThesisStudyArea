a6eaf14f75c742eb88bca9b04fb55c99436092d4
Sk Muhammad Asif <91278606+sheikhasif1996@users.noreply.github.com>
2024-01-22 20:07:27 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([38.638363, -90.279093], 8);
+
+  // load a tile layer
+ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
+
